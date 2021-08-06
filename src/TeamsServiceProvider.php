a18b0d07eee01d7b4e792fb78af869cb3573522e
Yuri Gerassimov <jurager01@gmail.com>
2021-08-06 16:06:16 +0600

    fix migrations

--- src/TeamsServiceProvider.php
+++ src/TeamsServiceProvider.php
@@ -51,8 +51,7 @@ class TeamsServiceProvider extends ServiceProvider
 
 		$this->publishes([
 			__DIR__.'/../database/migrations/2014_10_12_000000_create_users_table.php'           => database_path('migrations/2014_10_12_000000_create_users_table.php'),
-			__DIR__.'/../database/migrations/2020_05_21_100000_create_teams_table.php'           => database_path('migrations/2020_05_21_100000_create_teams_table.php'),
-			__DIR__.'/../database/migrations/2020_05_21_100000_create_teams_table.php'           => database_path('migrations/2020_05_21_100000_create_teams_table.php.php'),
+			__DIR__.'/../database/migrations/2019_01_19_100000_create_teams_table.php'           => database_path('migrations/2019_01_19_100000_create_teams_table.php'),
 			__DIR__.'/../database/migrations/2020_05_21_300000_create_invitations_table.php'     => database_path('migrations/2020_05_21_300000_create_invitations_table.php'),
 			__DIR__.'/../database/migrations/2020_05_21_300000_create_abilities_table.php'       => database_path('migrations/2020_05_21_300000_create_abilities_table.php'),
 			__DIR__.'/../database/migrations/2020_05_21_300000_create_permissions_table.php'     => database_path('migrations/2020_05_21_300000_create_permissions_table.php'),
