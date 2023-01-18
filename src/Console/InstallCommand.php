38efb7ade2d08b36e12ca418962dff3984c38f0e
Yuri Gerassimov <jurager01@gmail.com>
2023-01-18 22:19:35 +0600

    Fix issue with group model publishing
    Group model was not published during install command

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -43,10 +43,11 @@ class InstallCommand extends Command
 	    }
 
 	    copy(__DIR__.'/../../stubs/app/Models/Ability.php', app_path('Models/Ability.php'));
-	    copy(__DIR__.'/../../stubs/app/Models/Capability.php', app_path('Models/Capability.php'));
+        copy(__DIR__.'/../../stubs/app/Models/Capability.php', app_path('Models/Capability.php'));
+        copy(__DIR__.'/../../stubs/app/Models/Capability.php', app_path('Models/Group.php'));
+        copy(__DIR__.'/../../stubs/app/Models/Invitation.php', app_path('Models/Invitation.php'));
+        copy(__DIR__.'/../../stubs/app/Models/Membership.php', app_path('Models/Membership.php'));
 	    copy(__DIR__.'/../../stubs/app/Models/Permission.php', app_path('Models/Permission.php'));
-	    copy(__DIR__.'/../../stubs/app/Models/Membership.php', app_path('Models/Membership.php'));
-	    copy(__DIR__.'/../../stubs/app/Models/Invitation.php', app_path('Models/Invitation.php'));
 	    copy(__DIR__.'/../../stubs/app/Models/Role.php', app_path('Models/Role.php'));
 
 	    // Directories...
